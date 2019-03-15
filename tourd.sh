54919a4e50c27a81c8a9ff8b34f7a99e80e54ac2
Martin <m.schillhorn@gmail.com>
2019-03-15 12:06:46 +0100

    Update tourd.sh

--- tourd.sh
+++ tourd.sh
@@ -29,13 +29,13 @@ CONF='tour.conf'
 # Port.
 DEFAULT_PORT=5457
 # Explorer URL.
-EXPLORER_URL='https://explorer.tourcoin.info/'
+EXPLORER_URL='https://explorer.touriva.info/'
 # Rate limit explorer.
 EXPLORER_SLEEP=1
-EXPLORER_BLOCKCOUNT_PATH='api_fetch.php?method=getblockcount'
-EXPLORER_RAWTRANSACTION_PATH='/api_fetch.php?method=gettransaction&txid='
-EXPLORER_RAWTRANSACTION_PATH_SUFFIX=' '
-EXPLORER_GETADDRESS_PATH='api_fetch.php?method=getbalance&address='
+#EXPLORER_BLOCKCOUNT_PATH='api_fetch.php?method=getblockcount'
+#EXPLORER_RAWTRANSACTION_PATH='/api_fetch.php?method=gettransaction&txid='
+#EXPLORER_RAWTRANSACTION_PATH_SUFFIX=' '
+#EXPLORER_GETADDRESS_PATH='api_fetch.php?method=getbalance&address='
 # Amount of Collateral needed.
 COLLATERAL=500
 # Direct Daemon Download if github has no releases.
@@ -45,7 +45,7 @@ BLOCKTIME=60
 # Cycle Daemon on first start.
 DAEMON_CYCLE=1
 # Multiple on single IP.
-MULTI_IP_MODE=1
+MULTI_IP_MODE=0
 
 
 # Tip Address.
@@ -109,3 +109,4 @@ DAEMON_SETUP_THREAD
 # shellcheck source=/root/.bashrc
 . ~/.bashrc
 stty sane 2>/dev/null
+
